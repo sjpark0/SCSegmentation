342100b6d0a26711deb2b5d06305c3502d64cc15
sjpark0 <sjpark0@gmail.com>
2025-11-27 19:30:22 +0900

    SCMOT추가

--- SCSam2HQ/docker/launch_container.sh
+++ SCSam2HQ/docker/launch_container.sh
@@ -1,1 +1,1 @@
-docker run --shm-size=64g --gpus all -it --rm --env DISPLAY=$DISPLAY --env QT_X11_NO_MITSHM=1 --volume /tmp/.X11-unix:/tmp/.X11-unix --volume /:/host --workdir /host$PWD scsam2
+docker run --shm-size=64g --gpus all -it --rm --env DISPLAY=$DISPLAY --env QT_X11_NO_MITSHM=1 --volume /tmp/.X11-unix:/tmp/.X11-unix --volume /:/host --workdir /host$PWD scsam2hq
