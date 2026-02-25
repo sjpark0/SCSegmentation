ce810e849df45aed77a2d10566241c18360ce531
sjpark <sjpark0@gmail.com>
2026-02-25 15:48:31 +0900

    복귀

--- SCSam3/test/image_example.py
+++ SCSam3/test/image_example.py
@@ -8,7 +8,6 @@ import sys
 import os
 # if using Apple MPS, fall back to CPU for unsupported ops
 os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
-os.environ["HF_HOME"] = os.path.join(os.getcwd(), "../hf_cache")
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
